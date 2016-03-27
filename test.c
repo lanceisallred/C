cd70099a15efa95f21b571d8af40ff86e87a9881
lanceisallred <lallred@nevada.unr.edu>
2016-03-27 02:03:17 -0700

    Create test.c

---
+++ test.c
@@ -0,0 +1,21 @@
+//String Permutations
+
+#include <stdio.h>
+
+int main(void)
+{
+	char input[100];
+	int i = 0, j, len = 0;
+	
+	printf("Enter a string: ");
+	
+	while((input[i] = getchar()) != '\n'){
+		len++; i++;
+	}
+	printf("%d\n", len); //remove
+	for(i = 0; i < len; i++){
+		
+	}
+	
+	return 0;
+}
